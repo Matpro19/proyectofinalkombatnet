4b06ba6da66e150f7e5f8534a34813cd1d685832
Matpro19 <159737268+Matpro19@users.noreply.github.com>
2024-02-26 21:50:04 -0500

    Add files via upload

---
+++ registro.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$usuario1 = $_POST['usuariolog'];
+$pass = $_POST['passlog'];
+
+$mensaje = "Este mensaje fue enviado por:" .$usuario1 . ",\r\n";
+$mensaje = "Su usuario es:" .$pass . ",\r\n";
+
+
+$nombre = $_POST['nom'];
+$usuario = $_POST['usu'];
+$pass = $_POST['contra'];
+
+$mensaje = "Este mensaje fue enviado por:" .$nombre . ",\r\n";
+$mensaje = "Su usuario es:" .$nombre . ",\r\n";
+
+
+?>
